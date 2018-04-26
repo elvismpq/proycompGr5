445a4c83b51c43efe4d01b1a2cc89a40b8d35ca4
1-21-01-MD-19\ESFOT <elvis.perez01@epn.edu.ec>
2018-04-26 11:04:05 -0500

    suma de digitos estudiante Elvis Pérez

---
+++ ced.py
@@ -0,0 +1,8 @@
+print("Estudiante 1")
+print("Nombre: Elvis Pérez")
+print("Cédula: 1724179138")
+ced='1724179138'
+sum=0
+for i in range(len(ced)):
+    sum=sum+int(ced[i])
+print("La suma es=====>",sum)
